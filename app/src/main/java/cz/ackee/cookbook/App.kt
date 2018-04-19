ffd95d6cfed113f44351ef3905a32f5eac0d73ba
Pavel Skala <pavel.skala@ackee.cz>
2018-04-19 17:12:24 +0200

    Update versions, move fonts to resources, update some code

--- app/src/main/java/cz/ackee/cookbook/App.kt
+++ app/src/main/java/cz/ackee/cookbook/App.kt
@@ -10,14 +10,10 @@ class App : Application() {
 
     companion object {
         val TAG = App::class.java.name
-
-        private var context: Context? = null
     }
 
     override fun onCreate() {
         super.onCreate()
-
-        App.context = applicationContext
 
         // TODO initialize other libraries here
     }
