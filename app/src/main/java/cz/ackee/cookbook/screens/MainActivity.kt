25745038151d5f8dce7cf61a39dd0c105f2d2802
David Bilík <david.bilik@ackee.cz>
2019-02-18 08:40:26 +0100

    2019 Facelift: - Android X - New Kotlin version - Vector Drawables instead of pngs - Adaptive launcher icon - Update build.gradle to use newest stuff - Reformat project using Ackee codestyle - Small changes in Readme

--- app/src/main/java/cz/ackee/cookbook/screens/MainActivity.kt
+++ app/src/main/java/cz/ackee/cookbook/screens/MainActivity.kt
@@ -1,12 +1,12 @@
 package cz.ackee.cookbook.screens
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import cz.ackee.cookbook.R
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
 
